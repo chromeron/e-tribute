5d6496c7d654fe610df5ef394d56d7077c8a2ea0
Carlos Romero <juegos.lapyb@gmail.com>
2024-03-01 01:31:24 -0600

    post updated

--- src/pages/api/aws/upload.ts
+++ src/pages/api/aws/upload.ts
@@ -1,37 +1,37 @@
 import { createPresignedPost } from "./signUrl";
 import type { APIRoute } from "astro";
 
-export const POST: APIRoute = ({ request }) => {
-  return new Response(
-    JSON.stringify({
-      message: 'This was a POST!',
-    }),
-  );
-};
-
-
-// export const POST: APIRoute = async ({ request }) => {
-//   try {
-
-//     const formData = await request.formData();
-//     const contentType = formData.get("content-type");
-//     const key = formData.get("key");
-
-//     console.log("Key "+key+"  contentType: "+contentType);
-//     const data = await createPresignedPost({ key, contentType });
-//     return new Response(JSON.stringify({
-//       uploadUrl: data,
+// export const POST: APIRoute = ({ request }) => {
+//   return new Response(
+//     JSON.stringify({
+//       message: 'This was a POST!',
 //     }),
-//     { status: 200 });
-//   } catch (err) {
-//     console.error("Error in POST /upload", err);
-//     return new Response(JSON.stringify({
-//       error: err,
-//     }),
-//     { status: 500 });
-//   }
-  
+//   );
 // };
+
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+
+    const formData = await request.formData();
+    const contentType = formData.get("content-type");
+    const key = formData.get("key");
+
+    console.log("Key "+key+"  contentType: "+contentType);
+    const data = await createPresignedPost({ key, contentType });
+    return new Response(JSON.stringify({
+      uploadUrl: data,
+    }),
+    { status: 200 });
+  } catch (err) {
+    console.error("Error in POST /upload", err);
+    return new Response(JSON.stringify({
+      error: err,
+    }),
+    { status: 500 });
+  }
+  
+};
 
 export const GET: APIRoute = async ({ request }) => {
   try {
